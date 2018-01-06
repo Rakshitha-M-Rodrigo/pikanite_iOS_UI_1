d2fcf9012771ba2085bfcc67e8abebfb8be27067
Rakshitha Muranga Rodrigo <pathfinder.rover11b@gmail.com>
2018-01-06 15:39:25 +0530

    Initial commit

---
+++ pikanite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pikanite
+//
+//  Created by Rakshitha Muranga Rodrigo on 1/6/18.
+//  Copyright © 2018 Rakshitha Muranga Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
