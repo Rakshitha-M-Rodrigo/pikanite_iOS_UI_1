09713074b950b1b73ee92553187d5a6386413b73
Rakshitha Muranga Rodrigo <pathfinder.rover11b@gmail.com>
2018-01-07 14:11:26 +0530

    + slider the intro screen appear

--- pikanite/ViewController.swift
+++ pikanite/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        UIApplication.shared.statusBarStyle = .lightContent
         
     }
 
---
+++ pikanite/controllers/IntroViewController.swift
@@ -0,0 +1,81 @@
+//
+//  IntroViewController.swift
+//  pikanite
+//
+//  Created by Rakshitha Muranga Rodrigo on 1/7/18.
+//  Copyright © 2018 Rakshitha Muranga Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    //MARK: outlets
+    @IBOutlet weak var hiImageView: UIImageView!
+    @IBOutlet weak var hiLabel: UILabel!
+    @IBOutlet weak var hiDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var discoverImageView: UIImageView!
+    @IBOutlet weak var discoverLabel: UILabel!
+    @IBOutlet weak var discoverDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var bookLabel: UILabel!
+    @IBOutlet weak var bookDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var introControllerView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //animation
+        self.hiImageView.alpha = 0
+        self.hiLabel.alpha = 0
+        self.hiDescriptionLabel.alpha = 0
+        self.introControllerView.alpha = 0
+        self.hiLabel.font.withSize(1)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+            UIView.animate(withDuration: 3, animations: {
+                self.hiLabel.alpha = 1
+                self.hiLabel.font.withSize(40)
+            })
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: {
+                UIView.animate(withDuration: 2, animations: {
+                    self.hiDescriptionLabel.alpha = 1
+                })
+            })
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3), execute: {
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.hiImageView.alpha = 1
+                })
+            })
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(4), execute: {
+                    UIView.animate(withDuration: 0.1, animations: {
+                        self.introControllerView.alpha = 1
+                    })
+            })
+        })
+   
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
+
+extension IntroViewController: UIScrollViewDelegate{
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset
+        let percentage = (offset.x / (self.scrollView.frame.size.width  * 2) ) * 100
+        print(percentage)
+        
+    }
+}
